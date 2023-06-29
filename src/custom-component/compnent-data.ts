6573538d947ae6ef5b4e358fffd7cf2ef6047fab
leoyongyuan <1359195795@qq.com>
2023-06-29 11:56:09 +0800

    feat: 增加button数据和组件类型

---
+++ src/custom-component/compnent-data.ts
@@ -0,0 +1,52 @@
+import { componentDataType, commonAttrType } from "./type/index";
+const commonAttr: commonAttrType = {
+  animations: [],
+  events: {},
+  groupStyle: {}, // 当一个组件成为 Group 的子组件时使用
+  isLock: false, // 是否锁定组件
+  collapseName: "", // 编辑组件时记录当前使用的是哪个折叠面板，再次回来时恢复上次打开的折叠面板，优化用户体验
+  linkage: {
+    duration: 0, // 过渡持续时间
+    data: [
+      {
+        id: "", // 联动的组件 id
+        label: "", // 联动的组件名称
+        event: "", // 监听事件
+        style: [{ key: "", value: "" }], // 监听的事件触发时，需要改变的属性
+      },
+    ],
+  },
+};
+
+const list: componentDataType[] = [
+  {
+    component: "Button",
+    name: "按钮",
+    icon: "icon-button",
+    style: {
+      width: 100,
+      height: 34,
+      borderWidth: 1,
+      borderColor: "",
+      borderRadius: "",
+      fontSize: "",
+      fontWeight: 400,
+      lineHeight: "",
+      letterSpacing: 0,
+      textAlign: "",
+      color: "",
+      backgroundColor: "",
+    },
+    event: {
+      click: "",
+    },
+  },
+];
+
+for (let i = 0; i < list.length; i++) {
+  const item = list[i];
+  item.style = { ...item.style, ...commonAttr.groupStyle };
+  list[i] = { ...item, ...commonAttr };
+}
+
+export default list;
---
+++ src/custom-component/type/index.ts
@@ -0,0 +1,47 @@
+interface componentStyleType {
+  width?: number;
+  height?: number;
+  borderWidth?: number;
+  borderColor?: string;
+  borderRadius?: string;
+  fontSize?: string;
+  fontWeight?: number;
+  lineHeight?: string;
+  letterSpacing?: number;
+  textAlign?: string;
+  color?: string;
+  backgroundColor?: string;
+}
+
+interface componentEventType {
+  click?: string;
+}
+
+interface componentDataType {
+  component: string;
+  name: string;
+  icon: string;
+  style: componentStyleType;
+  event: componentEventType;
+}
+
+interface commonAttrType {
+  animations: string[];
+  events: componentEventType;
+  groupStyle: componentStyleType;
+  isLock: boolean;
+  collapseName: string;
+  linkage: {
+    duration: number;
+    data: [
+      {
+        id: string;
+        label: string;
+        event: string;
+        style: [{ key: string; value: string }];
+      }
+    ];
+  };
+}
+
+export type { componentDataType, componentStyleType, componentEventType, commonAttrType };
