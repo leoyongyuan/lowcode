56e348564290d27c309d0732bb786236f9542115
leoyongyuan <1359195795@qq.com>
2023-06-30 15:49:24 +0800

    feat: 引入新组件

--- src/custom-component/Button/App.tsx
+++ src/custom-component/Button/App.tsx
@@ -3,7 +3,13 @@ import { Button } from "antd";
 import { componentDataType } from "../../custom-component/type/index";
 const _Button = memo((props) => {
   const attr = props as componentDataType;
-  return <Button>{attr.name + attr.id}</Button>;
+  const style = attr.style as React.CSSProperties;
+  return (
+    <Button style={style}>
+      {attr.name}
+      {style.width}
+    </Button>
+  );
 });
 
 export default _Button;
---
+++ src/custom-component/Button/attr.tsx
@@ -0,0 +1,7 @@
+import React, { memo } from "react";
+
+const Attr = memo(() => {
+  return <div>Attr</div>;
+});
+
+export default Attr;
